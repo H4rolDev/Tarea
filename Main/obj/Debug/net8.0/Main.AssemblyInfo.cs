7080525900f90dd21e5dee4c9fdcf181aaa746f2
LAB_H_REAL <labh@continental.edu.pe>
2024-10-16 17:27:25 -0500

    jajaja

--- Main/obj/Debug/net8.0/Main.AssemblyInfo.cs
+++ Main/obj/Debug/net8.0/Main.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Main")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7b021747e795d70e379237b63bff6c95195eadc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d21babe3de8fca20456a265b1a0226357467984")]
 [assembly: System.Reflection.AssemblyProductAttribute("Main")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Main")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

907200e817efd4a8fa2a1d0ac57e6ecf378f53d3
Harol <harolsd121@gmail.com>
2024-10-18 23:03:22 -0500

    solution2

--- Main/obj/Debug/net6.0/Main.AssemblyInfo.cs
+++ Main/obj/Debug/net6.0/Main.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Main")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+355ff2e3a929528ec0384272713f2e7a84160cf1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50bf3e8f95cddded525ace0dc0437b3f0be835f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Main")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Main")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

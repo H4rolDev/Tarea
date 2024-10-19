f7efab8e5d2286cc062f259eda6d777a6762354e
Harol <harolsd121@gmail.com>
2024-10-19 00:57:15 -0500

    dasdasdasdasdap

--- DataBase/obj/Debug/net6.0/DataBase.AssemblyInfo.cs
+++ DataBase/obj/Debug/net6.0/DataBase.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataBase")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b1ab9bcd9428b17eede0572145eea6f381cf337")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fe9b67349a455dec946753375816583c8697f4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataBase")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataBase")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
